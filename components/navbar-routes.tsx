b1f0cc7f372078c2576bd0e015bdb3cf1bea0434
王晶 <1015861859@qq.com>
2023-10-11 22:41:41 +0800

    更新公共组件

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,32 +1,30 @@
 'use client'
 
 import { UserButton, useAuth } from '@clerk/nextjs'
-import { useRouter,usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import { LogOut } from 'lucide-react'
 import Link from "next/link";
+import { isMaster } from '@/lib/utils'
 
 const NavbarRoutes = () => {
 
   const pathname = usePathname()
-  const route = useRouter()
 
   const { userId } = useAuth();
 
   const isMasterPage = pathname?.includes('/master')
   const isPlayerPage = pathname?.includes('/player')
 
-  const isMaster=(id: string | null | undefined)=>{
-    console.log(id)
-    return true
-  }
+
   return (
     <div className='flex gap-x-2 ml-auto'>
       {isMasterPage||isPlayerPage?(
         <Link href='/'>
           <Button size='sm' variant='ghost'>
-            <LogOut className='h-4 w-4 mr-2'>退出</LogOut>
+            <LogOut className='h-4 w-4 mr-2' />
+            退出
           </Button>
         </Link>
       ):isMaster(userId)?(
